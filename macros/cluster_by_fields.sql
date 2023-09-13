0a104f464d3d42a5930ee34e11836d4a37010a63
Agnes Kiss <agnes@snowplowanalytics.com>
2023-09-13 09:18:49 +0100

    Rename web in macro

--- macros/cluster_by_fields.sql
+++ macros/cluster_by_fields.sql
@@ -5,76 +5,76 @@ and you may not use this file except in compliance with the Snowplow Community L
 You may obtain a copy of the Snowplow Community License Version 1.0 at https://docs.snowplow.io/community-license-1.0
 #}
 
-{% macro web_cluster_by_fields_sessions_lifecycle() %}
+{% macro unified_cluster_by_fields_sessions_lifecycle() %}
 
-  {{ return(adapter.dispatch('web_cluster_by_fields_sessions_lifecycle', 'snowplow_unified')()) }}
+  {{ return(adapter.dispatch('unified_cluster_by_fields_sessions_lifecycle', 'snowplow_unified')()) }}
 
 {% endmacro %}
 
-{% macro default__web_cluster_by_fields_sessions_lifecycle() %}
+{% macro default__unified_cluster_by_fields_sessions_lifecycle() %}
 
   {{ return(snowplow_utils.get_value_by_target_type(bigquery_val=["session_identifier"], snowflake_val=["to_date(start_tstamp)"])) }}
 
 {% endmacro %}
 
 
-{% macro web_cluster_by_fields_views() %}
+{% macro unified_cluster_by_fields_views() %}
 
-  {{ return(adapter.dispatch('web_cluster_by_fields_views', 'snowplow_unified')()) }}
+  {{ return(adapter.dispatch('unified_cluster_by_fields_views', 'snowplow_unified')()) }}
 
 {% endmacro %}
 
-{% macro default__web_cluster_by_fields_views() %}
+{% macro default__unified_cluster_by_fields_views() %}
 
   {{ return(snowplow_utils.get_value_by_target_type(bigquery_val=["user_identifier","session_identifier"], snowflake_val=["to_date(start_tstamp)"])) }}
 
 {% endmacro %}
 
 
-{% macro web_cluster_by_fields_sessions() %}
+{% macro unified_cluster_by_fields_sessions() %}
 
-  {{ return(adapter.dispatch('web_cluster_by_fields_sessions', 'snowplow_unified')()) }}
+  {{ return(adapter.dispatch('unified_cluster_by_fields_sessions', 'snowplow_unified')()) }}
 
 {% endmacro %}
 
-{% macro default__web_cluster_by_fields_sessions() %}
+{% macro default__unified_cluster_by_fields_sessions() %}
 
   {{ return(snowplow_utils.get_value_by_target_type(bigquery_val=["domain_userid"], snowflake_val=["to_date(start_tstamp)"])) }}
 
 {% endmacro %}
 
 
-{% macro web_cluster_by_fields_users() %}
+{% macro unified_cluster_by_fields_users() %}
 
-  {{ return(adapter.dispatch('web_cluster_by_fields_users', 'snowplow_unified')()) }}
+  {{ return(adapter.dispatch('unified_cluster_by_fields_users', 'snowplow_unified')()) }}
 
 {% endmacro %}
 
-{% macro default__web_cluster_by_fields_users() %}
+{% macro default__unified_cluster_by_fields_users() %}
 
   {{ return(snowplow_utils.get_value_by_target_type(bigquery_val=["user_id","domain_userid"], snowflake_val=["to_date(start_tstamp)"])) }}
 
 {% endmacro %}
 
-{% macro web_cluster_by_fields_consent() %}
+{% macro unified_cluster_by_fields_consent() %}
 
-  {{ return(adapter.dispatch('web_cluster_by_fields_consent', 'snowplow_unified')()) }}
+  {{ return(adapter.dispatch('unified_cluster_by_fields_consent', 'snowplow_unified')()) }}
 
 {% endmacro %}
 
-{% macro default__web_cluster_by_fields_consent() %}
+{% macro default__unified_cluster_by_fields_consent() %}
 
   {{ return(snowplow_utils.get_value_by_target_type(bigquery_val=["event_id","domain_userid"], snowflake_val=["to_date(load_tstamp)"])) }}
 
 {% endmacro %}
 
-{% macro web_cluster_by_fields_cwv() %}
+{% macro unified_cluster_by_fields_cwv() %}
 
-  {{ return(adapter.dispatch('web_cluster_by_fields_cwv', 'snowplow_unified')()) }}
+  {{ return(adapter.dispatch('unified_cluster_by_fields_cwv', 'snowplow_unified')()) }}
 
 {% endmacro %}
 
-{% macro default__web_cluster_by_fields_cwv() %}
+{% macro default__unified_cluster_by_fields_cwv() %}
 
   {{ return(snowplow_utils.get_value_by_target_type(bigquery_val=["view_id","domain_userid"], snowflake_val=["to_date(derived_tstamp)"])) }}
 
