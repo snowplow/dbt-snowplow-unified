5ca4262348894bc615d934d3b41958e27ff4b40c
Agnes Kiss <95634439+agnessnowplow@users.noreply.github.com>
2023-09-13 17:11:04 +0100

    Update macros/field_extractions/get_browser_context_fields.sql
    Co-authored-by: Emiel Verkade <emiel.verkade@gmail.com>

--- macros/field_extractions/get_browser_context_fields.sql
+++ macros/field_extractions/get_browser_context_fields.sql
@@ -74,7 +74,7 @@ You may obtain a copy of the Snowplow Community License Version 1.0 at https://d
     , cast(null as {{ type_string() }}) as browser__resolution
     , cast(null as {{ type_string() }}) as browser__color_depth
     , cast(null as {{ type_string() }}) as browser__device_pixel_ratio
-    , cast(null as {{ type_string() }}) as browser__cookies_enabled
+    , cast(null as {{ type_boolean() }}) as browser__cookies_enabled
     , cast(null as {{ type_boolean() }}) as browser__online
     , cast(null as {{ type_string() }}) as browser__browser_language
     , cast(null as {{ type_string() }}) as browser__document_language
