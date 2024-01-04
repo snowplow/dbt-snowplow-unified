559eef8145d795894f72f4f67fc481a189040b36
Matúš Tomlein <matus.tomlein@gmail.com>
2024-01-04 12:35:35 +0100

    Fix copyright year for new files

--- integration_tests/models/expected/snowplow_unified_views_mobile_screen_engagement_expected_stg.sql
+++ integration_tests/models/expected/snowplow_unified_views_mobile_screen_engagement_expected_stg.sql
@@ -1,5 +1,5 @@
 {#
-Copyright (c) 2023-present Snowplow Analytics Ltd. All rights reserved.
+Copyright (c) 2024-present Snowplow Analytics Ltd. All rights reserved.
 This program is licensed to you under the Snowplow Personal and Academic License Version 1.0,
 and you may not use this file except in compliance with the Snowplow Personal and Academic License Version 1.0.
 You may obtain a copy of the Snowplow Personal and Academic License Version 1.0 at https://docs.snowplow.io/personal-and-academic-license-1.0/
