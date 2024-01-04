327a83f6658da116eee72b9b5c5b7c6d1d3f19f1
Matúš Tomlein <matus.tomlein@gmail.com>
2024-01-04 14:10:45 +0100

    Fix screen summary JSON context parsing on Redshift

--- integration_tests/models/source/default/snowplow_unified_screen_summary_context_stg.sql
+++ integration_tests/models/source/default/snowplow_unified_screen_summary_context_stg.sql
@@ -1,28 +1,44 @@
-  {#
+{#
 Copyright (c) 2024-present Snowplow Analytics Ltd. All rights reserved.
 This program is licensed to you under the Snowplow Personal and Academic License Version 1.0,
 and you may not use this file except in compliance with the Snowplow Personal and Academic License Version 1.0.
 You may obtain a copy of the Snowplow Personal and Academic License Version 1.0 at https://docs.snowplow.io/personal-and-academic-license-1.0/
 #}
 
+{% set col = 'ev.contexts_com_snowplowanalytics_mobile_screen_summary_1_0_0' %}
+
 Select
   ev.event_id as root_id,
   ev.collector_tstamp::timestamp as root_tstamp,
 
-  (ev.contexts_com_snowplowanalytics_mobile_screen_summary_1_0_0::json->0 ->>'foreground_sec')::float as foreground_sec,
-  (ev.contexts_com_snowplowanalytics_mobile_screen_summary_1_0_0::json->0 ->>'background_sec')::float as background_sec,
-
-  (ev.contexts_com_snowplowanalytics_mobile_screen_summary_1_0_0::json->0 ->>'last_item_index')::int as last_item_index,
-  (ev.contexts_com_snowplowanalytics_mobile_screen_summary_1_0_0::json->0 ->>'items_count')::int as items_count,
-
-  (ev.contexts_com_snowplowanalytics_mobile_screen_summary_1_0_0::json->0 ->>'max_x_offset')::int as max_x_offset,
-  (ev.contexts_com_snowplowanalytics_mobile_screen_summary_1_0_0::json->0 ->>'min_x_offset')::int as min_x_offset,
-
-  (ev.contexts_com_snowplowanalytics_mobile_screen_summary_1_0_0::json->0 ->>'max_y_offset')::int as max_y_offset,
-  (ev.contexts_com_snowplowanalytics_mobile_screen_summary_1_0_0::json->0 ->>'min_y_offset')::int as min_y_offset,
-
-  (ev.contexts_com_snowplowanalytics_mobile_screen_summary_1_0_0::json->0 ->>'content_height')::int as content_height,
-  (ev.contexts_com_snowplowanalytics_mobile_screen_summary_1_0_0::json->0 ->>'content_width')::int as content_width,
+  {% for (att, type) in [
+    ('foreground_sec', 'float'),
+    ('background_sec', 'float'),
+    ('last_item_index', 'int'),
+    ('items_count', 'int'),
+    ('max_x_offset', 'int'),
+    ('min_x_offset', 'int'),
+    ('max_y_offset', 'int'),
+    ('min_y_offset', 'int'),
+    ('content_height', 'int'),
+    ('content_width', 'int'),
+  ] %}
+    {% if target.type == 'postgres' -%}
+      (
+        {{ col }}::json->0 ->>'{{ att }}'
+      )::{{ type}} as {{ att }},
+    {%- else -%}
+      case when {{ col }} like '%{{ att }}%' then
+        JSON_EXTRACT_PATH_TEXT(
+          JSON_EXTRACT_ARRAY_ELEMENT_TEXT(
+            {{ col }},
+            0
+          ),
+          '{{ att }}'
+        )::{{ type }}
+      end as {{ att }},
+    {%- endif %}
+  {% endfor %}
 
   'screen_summary' as schema_name
 
