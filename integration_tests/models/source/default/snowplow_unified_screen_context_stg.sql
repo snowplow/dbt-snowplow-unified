4466ec42d408402aacdc247a04e5326ab7de1518
Matus Tomlein <matus.tomlein@gmail.com>
2024-01-04 12:22:04 +0100

    Consider screen view ID from the screen view context (close #14)
    PR #15

--- integration_tests/models/source/default/snowplow_unified_screen_context_stg.sql
+++ integration_tests/models/source/default/snowplow_unified_screen_context_stg.sql
@@ -5,16 +5,27 @@ and you may not use this file except in compliance with the Snowplow Personal an
 You may obtain a copy of the Snowplow Personal and Academic License Version 1.0 at https://docs.snowplow.io/personal-and-academic-license-1.0/
 #}
 
+with events as (
+  select event_id, collector_tstamp, platform, contexts_com_snowplowanalytics_mobile_screen_1_0_0
+  from {{ ref('snowplow_unified_events') }}
+)
+
 select
-  event_id as root_id,
-  collector_tstamp::timestamp as root_tstamp,
-  case when platform = 'web' then null else '4e8c2289-b1cd-4915-90de-2d87e1976a58' end as id,
-  case when platform = 'web' then null else 'Add New Item' end as name,
-  case when platform = 'web' then null else 'na' end as activity,
-  case when platform = 'web' then null else 'na' end as type,
-  case when platform = 'web' then null else 'na' end as fragment,
-  case when platform = 'web' then null else 'na' end as top_view_controller,
-  case when platform = 'web' then null else 'na' end as view_controller,
+  ev.event_id as root_id,
+  ev.collector_tstamp::timestamp as root_tstamp,
+  case when ev.platform = 'web' then null else
+    {% if target.type == 'postgres' -%}
+    JSON_EXTRACT_PATH_TEXT(contexts_com_snowplowanalytics_mobile_screen_1_0_0::json, '0', 'id')
+    {%- else -%}
+    JSON_EXTRACT_PATH_TEXT(JSON_EXTRACT_ARRAY_ELEMENT_TEXT(contexts_com_snowplowanalytics_mobile_screen_1_0_0, 0), 'id')
+    {%- endif %}
+  end as id,
+  case when ev.platform = 'web' then null else 'Add New Item' end as name,
+  case when ev.platform = 'web' then null else 'na' end as activity,
+  case when ev.platform = 'web' then null else 'na' end as type,
+  case when ev.platform = 'web' then null else 'na' end as fragment,
+  case when ev.platform = 'web' then null else 'na' end as top_view_controller,
+  case when ev.platform = 'web' then null else 'na' end as view_controller,
   'screen_context' as schema_name
 
-from {{ ref('snowplow_unified_events') }}
+from events as ev
