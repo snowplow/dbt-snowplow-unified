c2cf8160bf208ccff0b665c2d223984a9df785f6
Ryan Hill <r.l.hill128@gmail.com>
2024-02-12 14:01:30 +0000

    Move cluster by to macros to allow users to overwrite (Close #11)

--- models/optional_modules/core_web_vitals/snowplow_unified_web_vitals.sql
+++ models/optional_modules/core_web_vitals/snowplow_unified_web_vitals.sql
@@ -18,7 +18,7 @@ You may obtain a copy of the Snowplow Personal and Academic License Version 1.0 
       "field": "derived_tstamp",
       "data_type": "timestamp"
     }, databricks_val = 'derived_tstamp_date'),
-    cluster_by=snowplow_utils.get_value_by_target_type(bigquery_val=["view_id","user_identifier"], snowflake_val=["to_date(derived_tstamp)"]),
+    cluster_by=snowplow_unified.unified_cluser_by('web_vitals'),
     sql_header=snowplow_utils.set_query_tag(var('snowplow__query_tag', 'snowplow_dbt')),
     tblproperties={
       'delta.autoOptimize.optimizeWrite' : 'true',
