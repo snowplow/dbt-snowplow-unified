e2bb39d157fdc96446687f5c2ca5b7a65716c5ea
Agnes Kiss <agnes@snowplowanalytics.com>
2024-06-12 10:16:58 +0100

    Fix Spark field extractions

--- macros/field_extractions/get_screen_context_fields.sql
+++ macros/field_extractions/get_screen_context_fields.sql
@@ -54,13 +54,13 @@ You may obtain a copy of the Snowplow Personal and Academic License Version 1.0 
 
 {% macro spark__get_screen_context_fields() %}
   {% if var('snowplow__enable_screen_context', false) %}
-    , contexts_com_snowplowanalytics_mobile_screen_1[0].id::STRING AS screen__id
-    , contexts_com_snowplowanalytics_mobile_screen_1[0].name::STRING AS screen__name
-    , contexts_com_snowplowanalytics_mobile_screen_1[0].activity::STRING AS screen__activity
-    , contexts_com_snowplowanalytics_mobile_screen_1[0].fragment::STRING AS screen__fragment
-    , contexts_com_snowplowanalytics_mobile_screen_1[0].top_view_controller::STRING AS screen__top_view_controller
-    , contexts_com_snowplowanalytics_mobile_screen_1[0].type::STRING AS screen__type
-    , contexts_com_snowplowanalytics_mobile_screen_1[0].view_controller::STRING AS screen__view_controller
+    , cast(contexts_com_snowplowanalytics_mobile_screen_1[0].id as STRING) AS screen__id
+    , cast(contexts_com_snowplowanalytics_mobile_screen_1[0].name as STRING) AS screen__name
+    , cast(contexts_com_snowplowanalytics_mobile_screen_1[0].activity as STRING) AS screen__activity
+    , cast(contexts_com_snowplowanalytics_mobile_screen_1[0].fragment as STRING) AS screen__fragment
+    , cast(contexts_com_snowplowanalytics_mobile_screen_1[0].top_view_controller as STRING) AS screen__top_view_controller
+    , cast(contexts_com_snowplowanalytics_mobile_screen_1[0].type as STRING) AS screen__type
+    , cast(contexts_com_snowplowanalytics_mobile_screen_1[0].view_controller as STRING) AS screen__view_controller
   {% else %}
     , cast(null as {{ dbt.type_string() }}) as screen__id
     , cast(null as {{ dbt.type_string() }}) as screen__name
