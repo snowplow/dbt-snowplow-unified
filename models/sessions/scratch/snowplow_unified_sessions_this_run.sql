2adb79d38a4269fd6362fbf14bf840fe83a9d1ce
TsungYu(James) Chien <james.chien@viki.com>
2024-06-12 17:02:02 +0800

    fix: solve the remaing issue

--- models/sessions/scratch/snowplow_unified_sessions_this_run.sql
+++ models/sessions/scratch/snowplow_unified_sessions_this_run.sql
@@ -64,7 +64,7 @@ with session_firsts as (
           {{ snowplow_unified.mobile_context_fields('ev')}}
         {% endif %}
 
-        {% if target.type == 'postgres' %}
+        {% if target.type in ['spark', 'postgres'] %}
           , row_number() over (partition by ev.session_identifier order by ev.derived_tstamp, ev.dvce_created_tstamp, ev.event_id) as session_dedupe_index
         {% endif %}
 
@@ -86,7 +86,7 @@ with session_firsts as (
 
     from {{ ref('snowplow_unified_events_this_run') }} ev
     left join
-        {{ ref(var('snowplow__ga4_categories_seed')) }} c on 
+        {{ ref(var('snowplow__ga4_categories_seed')) }} c on
         {% if var('snowplow__use_refr_if_mkt_null', false) %}
         lower(trim(coalesce(ev.mkt_source, ev.refr_source)) = lower(c.source)
         {% else %}
@@ -103,7 +103,7 @@ with session_firsts as (
       {{ snowplow_unified.filter_bots() }}
     {% endif %}
 
-    {% if target.type not in ['postgres'] %}
+    {% if target.type not in ['spark', 'postgres'] %}
       qualify row_number() over (partition by session_identifier order by derived_tstamp, dvce_created_tstamp, event_id) = 1
     {% endif %}
 )
@@ -136,7 +136,7 @@ with session_firsts as (
         ev.screen_view__type as last_screen_view__type,
       {% endif %}
 
-      {% if target.type == 'postgres' %}
+      {% if target.type in ['postgres', 'spark'] %}
         row_number() over (partition by ev.session_identifier order by ev.derived_tstamp desc, ev.dvce_created_tstamp desc, ev.event_id) AS session_dedupe_index,
       {% endif %}
 
@@ -153,8 +153,7 @@ with session_firsts as (
         {% if var("snowplow__ua_bot_filter", true) %}
             {{ snowplow_unified.filter_bots() }}
         {% endif %}
-
-    {% if target.type not in ['postgres'] %}
+    {% if target.type not in ['postgres', 'spark'] %}
       qualify row_number() over (partition by session_identifier order by derived_tstamp desc, dvce_created_tstamp desc, event_id) = 1
     {% endif %}
 )
@@ -461,7 +460,7 @@ from session_firsts f
 left join session_lasts l
 on f.session_identifier = l.session_identifier
 
-{% if target.type == 'postgres' %}
+{% if target.type in ['spark', 'postgres'] %}
   and l.session_dedupe_index = 1
 {%- endif %}
 
@@ -472,6 +471,6 @@ on f.session_identifier = a.session_identifier
 left join session_convs d on f.session_identifier = d.session_identifier
 {%- endif %}
 
-{% if target.type == 'postgres' %}
+{% if target.type in ['spark', 'postgres'] %}
    where f.session_dedupe_index = 1
 {%- endif %}
