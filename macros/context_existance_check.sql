053130197dfd4736695c1338c4af180183d084c8
Ilias Xenogiannis <ilias1111@users.noreply.github.com>
2024-06-14 16:12:23 +0300

    Update context_existance_check.sql

--- macros/context_existance_check.sql
+++ macros/context_existance_check.sql
@@ -63,7 +63,6 @@ You may obtain a copy of the Snowplow Personal and Academic License Version 1.0 
     {%- if flags.WHICH in ('run', 'run-operation') -%}
 
       {% set relation = adapter.get_relation(
-          database=target.database,
           database= var('snowplow__database', target.database) if target.type not in ['databricks', 'spark'] else var('snowplow__databricks_catalog', 'hive_metastore') if target.type in ['databricks'] else var('snowplow__atomic_schema', 'atomic'),
           schema=var('snowplow__atomic_schema', 'atomic'),
           identifier=var('snowplow__events_table', 'events'))
