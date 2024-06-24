27b2f04f09a79b980b144845d353c661630edb61
Agnes Kiss <agnes@snowplowanalytics.com>
2024-06-24 12:14:17 +0100

    Restructure user sessions this run

--- models/users/scratch/snowplow_unified_users_sessions_this_run.sql
+++ models/users/scratch/snowplow_unified_users_sessions_this_run.sql
@@ -12,21 +12,15 @@ You may obtain a copy of the Snowplow Personal and Academic License Version 1.0 
   )
 }}
 
-with prep as (
-  select
-    a.*,
-    case when platform = 'web' then true else false end on_web_base,
-    case when platform <> 'web' then true else false end on_mobile_base,
-    min(a.start_tstamp) over(partition by a.user_identifier) as user_start_tstamp,
-    max(a.end_tstamp) over(partition by a.user_identifier) as user_end_tstamp
 
-  from {{ var('snowplow__sessions_table') }} a
-  where exists (select 1 from {{ ref('snowplow_unified_base_sessions_this_run') }} b where a.user_identifier = b.user_identifier)
+select
+  a.*,
+  case when platform = 'web' then true else false end on_web_base,
+  case when platform <> 'web' then true else false end on_mobile_base,
+  min(a.start_tstamp) over(partition by a.user_identifier) as user_start_tstamp,
+  max(a.end_tstamp) over(partition by a.user_identifier) as user_end_tstamp,
+  cast(max(cast(case when platform = 'web' then true else false end as {{ dbt.type_int() }}))  over(partition by user_identifier) as {{ dbt.type_boolean() }}) as on_web,
+  cast(max(cast(case when platform <> 'web' then true else false end as {{ dbt.type_int() }})) over(partition by user_identifier) as {{ dbt.type_boolean() }}) as on_mobile
 
-)
-
-select *,
-  cast(max(cast(on_web_base as {{ dbt.type_int() }}))  over(partition by user_identifier) as {{ dbt.type_boolean() }}) as on_web,
-  cast(max(cast(on_mobile_base as {{ dbt.type_int() }})) over(partition by user_identifier) as {{ dbt.type_boolean() }}) as on_mobile
-
-from prep
+from {{ var('snowplow__sessions_table') }} a
+where exists (select 1 from {{ ref('snowplow_unified_base_sessions_this_run') }} b where a.user_identifier = b.user_identifier)
