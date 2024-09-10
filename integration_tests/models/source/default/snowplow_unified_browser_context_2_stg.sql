2001efd22238aacbf1226d24f5d91307606d4af2
Ilias Xenogiannis <ilias1111@users.noreply.github.com>
2024-09-10 15:14:50 +0300

    Basic test

--- integration_tests/models/source/default/snowplow_unified_browser_context_2_stg.sql
+++ integration_tests/models/source/default/snowplow_unified_browser_context_2_stg.sql
@@ -5,12 +5,6 @@ and you may not use this file except in compliance with the Snowplow Personal an
 You may obtain a copy of the Snowplow Personal and Academic License Version 1.0 at https://docs.snowplow.io/personal-and-academic-license-1.0/
 #}
 
-{{ 
-  config(
-    enabled=var('snowplow__enable_browser_context_2',false),
-  ) 
-}}
-
 Select
 
     event_id as root_id,
