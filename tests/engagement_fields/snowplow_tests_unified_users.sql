4232cafa7ad700891077097c64e347e45e437267
Oscar <oscar.freeman@snowplowanalytics.com>
2024-09-23 14:16:19 +0100

    almost complete maybe there will be minor alterations

---
+++ tests/engagement_fields/snowplow_tests_unified_users.sql
@@ -0,0 +1,14 @@
+{#
+Copyright (c) 2023-present Snowplow Analytics Ltd. All rights reserved.
+This program is licensed to you under the Snowplow Community License Version 1.0,
+and you may not use this file except in compliance with the Snowplow Community License Version 1.0.
+You may obtain a copy of the Snowplow Community License Version 1.0 at https://docs.snowplow.io/community-license-1.0
+#}
+
+select *
+from {{ ref('snowplow_unified_users') }}
+where user_identifier is null --cannot be null from other tests
+{% if var('snowplow__enable_web') or var('snowplow__enable_screen_summary_context', false) %}
+      or engaged_time_in_s is null -- where there are no pings, engaged time is 0.
+      --or absolute_time_in_s is null 
+{% endif %}
