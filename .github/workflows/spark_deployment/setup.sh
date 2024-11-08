d8b38149434ed4c186035568e308bf26a9989a6c
Ilias Xenogiannis <ilias1111@users.noreply.github.com>
2024-11-08 18:09:50 +0200

    Token addition

--- .github/workflows/spark_deployment/setup.sh
+++ .github/workflows/spark_deployment/setup.sh
@@ -4,6 +4,7 @@
 # Create a new spark-defaults.conf with substituted values
 sed -e "s|\${AWS_ACCESS_KEY_ID}|$AWS_ACCESS_KEY_ID|g" \
     -e "s|\${AWS_SECRET_ACCESS_KEY}|$AWS_SECRET_ACCESS_KEY|g" \
+    -e "s|\${AWS_SESSION_TOKEN}|$AWS_SESSION_TOKEN|g" \
     /spark/conf/spark-defaults.conf.template > /spark/conf/spark-defaults.conf
 # Execute the passed command
 exec "$@"
