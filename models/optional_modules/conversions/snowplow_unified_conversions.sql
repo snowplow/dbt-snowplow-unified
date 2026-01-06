cdb62d05de87974c543b2971885e153e7e3cac03
Agnes Kiss <agnes@snowplowanalytics.com>
2026-01-06 15:10:47 +0000

    Change yml with autofix

--- models/optional_modules/conversions/snowplow_unified_conversions.sql
+++ models/optional_modules/conversions/snowplow_unified_conversions.sql
@@ -5,32 +5,29 @@ and you may not use this file except in compliance with the Snowplow Personal an
 You may obtain a copy of the Snowplow Personal and Academic License Version 1.0 at https://docs.snowplow.io/personal-and-academic-license-1.0/
 #}
 
-{{
-  config(
-    materialized='incremental',
-    enabled=var("snowplow__enable_conversions", false),
-    on_schema_change='append_new_columns',
-    unique_key='cv_id',
-    upsert_date_key='cv_tstamp',
-    sort='cv_tstamp',
-    dist='cv_id',
-    partition_by = snowplow_utils.get_value_by_target_type(bigquery_val = {
+{{ config(
+    materialized="incremental", 
+    enabled=var("snowplow__enable_conversions", false), 
+    on_schema_change="append_new_columns", 
+    unique_key="cv_id", 
+    sort="cv_tstamp", 
+    dist="cv_id", 
+    partition_by=snowplow_utils.get_value_by_target_type(bigquery_val = {
       "field": "cv_tstamp",
       "data_type": "timestamp"
-    }, databricks_val='cv_tstamp_date'),
-    cluster_by=snowplow_unified.get_cluster_by_values('conversions'),
-    tags=["derived"],
+    }, databricks_val='cv_tstamp_date'), 
+    cluster_by=snowplow_unified.get_cluster_by_values('conversions'), 
+    tags=["derived"], 
     post_hook="{{ snowplow_unified.stitch_user_identifiers(
       enabled=var('snowplow__conversion_stitching')
-      ) }}",
-    sql_header=snowplow_utils.set_query_tag(var('snowplow__query_tag', 'snowplow_dbt')),
+      ) }}", 
+    sql_header=snowplow_utils.set_query_tag(var('snowplow__query_tag', 'snowplow_dbt')), 
     tblproperties={
       'delta.autoOptimize.optimizeWrite' : 'true',
       'delta.autoOptimize.autoCompact' : 'true'
-    },
-    snowplow_optimize = true
-  )
-}}
+    }, 
+    meta={'upsert_date_key': 'cv_tstamp', 'snowplow_optimize': true}
+) }}
 
 
 select *
--- models/optional_modules/core_web_vitals/snowplow_unified_web_vitals.sql
+++ models/optional_modules/core_web_vitals/snowplow_unified_web_vitals.sql
@@ -5,28 +5,25 @@ and you may not use this file except in compliance with the Snowplow Personal an
 You may obtain a copy of the Snowplow Personal and Academic License Version 1.0 at https://docs.snowplow.io/personal-and-academic-license-1.0/
 #}
 
-{{
-  config(
-    materialized= 'incremental',
-    enabled=var("snowplow__enable_cwv", false) | as_bool(),
-    unique_key='view_id',
-    upsert_date_key='derived_tstamp',
-    sort='derived_tstamp',
-    dist='view_id',
-    tags=["derived"],
-    partition_by = snowplow_utils.get_value_by_target_type(bigquery_val = {
+{{ config(
+    materialized="incremental", 
+    enabled=var("snowplow__enable_cwv", false) | as_bool(), 
+    unique_key="view_id", 
+    sort="derived_tstamp", 
+    dist="view_id", 
+    tags=["derived"], 
+    partition_by=snowplow_utils.get_value_by_target_type(bigquery_val = {
       "field": "derived_tstamp",
       "data_type": "timestamp"
-    }, databricks_val = 'derived_tstamp_date'),
-    cluster_by=snowplow_unified.get_cluster_by_values('web_vitals'),
-    sql_header=snowplow_utils.set_query_tag(var('snowplow__query_tag', 'snowplow_dbt')),
+    }, databricks_val = 'derived_tstamp_date'), 
+    cluster_by=snowplow_unified.get_cluster_by_values('web_vitals'), 
+    sql_header=snowplow_utils.set_query_tag(var('snowplow__query_tag', 'snowplow_dbt')), 
     tblproperties={
       'delta.autoOptimize.optimizeWrite' : 'true',
       'delta.autoOptimize.autoCompact' : 'true'
-    },
-    snowplow_optimize= true
-  )
-}}
+    }, 
+    meta={'upsert_date_key': 'derived_tstamp', 'snowplow_optimize': true}
+) }}
 
 select
   *
